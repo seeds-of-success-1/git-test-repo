cdce9b5ef1eaf6be745ef4692e250eb9c584a70f
DougCaswell <dougcaswell@gmail.com>
2019-01-25 15:43:23 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dcTest from './components/dc-test';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Git Test Repo</h1>
+        <h1>Git Testing Repo</h1>
+
       </div>
     );
   }
---
+++ src/components/dc-test.js
@@ -0,0 +1,5 @@
+export default {
+    function render() {
+        return (<div>Hi</div>)
+    }
+}
